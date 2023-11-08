249772e0d46e17b3dc382bce41d3507d1ab36fed
Danil <danil.kotliarov@yandex.ru>
2023-11-08 17:23:32 +1000

    add new files

---
+++ webSocketClient/webSocketClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webSocketClient
+//
+//  Created by Danil on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ webSocketClient/webSocketClient/WebSocket.swift
@@ -0,0 +1,8 @@
+//
+//  WebSocket.swift
+//  webSocketClient
+//
+//  Created by Danil on 06.11.2023.
+//
+
+import Foundation
